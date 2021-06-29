459e7b7557c63badce67545717042997e735bfce
Trausti Dagsson <traustid@gmail.com>
2021-06-29 08:23:24 +0000

    löguð slóð á myndir

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "//lexia.arnastofnun.is/precache-manifest.a0810db8cae093ef897b0e853b82a8a9.js"
+  "//lexia.arnastofnun.is/precache-manifest.4c1420f650eeb8b793b581b7dd201946.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("//lexia.arnastofnun.is/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
